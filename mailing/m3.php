4e2aa35b0617bbcd4fa195260925fbccd3559254
Hasin Hayder <countdraculla@gmail.com>
2019-03-30 21:20:29 +0600

    PHp Course

--- mailing/m3.php
+++ mailing/m3.php
@@ -1,25 +1,34 @@
 <?php
+require_once("phpmailer/PHPmailer.php");
+require_once("phpmailer/Exception.php");
+require_once("phpmailer/SMTP.php");
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once("phpmailer/PHPMailer.php");
-require_once("phpmailer/Exception.php");
-
-$pm = new PHPMailer();
+$pm = new PHPMailer(true);
 
 try{
-    $pm->setFrom('test@example.com');
-    $pm->addAddress('augustwind16@gmail.com');
-    $pm->addReplyTo('john@doe.com');
-    $pm->addCC('john@doe.com');
-    $pm->addBCC('john@doe.com');
-    $pm->Subject = "Here is the poem for you";
-    $pm->Body = "Hi, Here is the <strong>poem</strong>";
-    $pm->AltBody = "Here is the poem in text";
+
+    $pm->SMTPDebug = 2;
+    $pm->isSMTP(true);
+    $pm->Host = "mail.server.com";
+    $pm->SMTPAuth = true;
+    $pm->Username = "youremail@domain.com";
+    $pm->Password = "*********";
+    $pm->SMTPSecure = "tls";
+    $pm->Port = 587;
+
+    $pm->setFrom("test@hasin.me");
+    $pm->addAddress("receiver@domain.com");
+    $pm->Subject = "Here is the invoice";
+    $pm->Body = "Hi, Here is the <strong>invoice</strong> from your last purchase";
+    $pm->AltBody = "Here is the invoice from your last purchase";
     $pm->isHTML(true);
-    $pm->addAttachment("/Applications/AMPPS/www/php.local.com/mailing/sick-poem.php");
+    $pm->addAttachment("/Applications/AMPPS/www/php.local.com/mailing/sick-poem.pdf");
     $pm->send();
+
     echo "Mail Sent";
-}catch(Exception $e){
-    echo "Mail not sent";
+}catch (Exception $e){
+    echo "Failed ".$pm->ErrorInfo;
 }
